d49ddd993412242fd0a4393269dc1e308a57e003
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-10-31 14:56:26 +0400

    + node types
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- py/xl.py
+++ py/xl.py
@@ -1,4 +1,57 @@
 ## @file
 ## @brief XL language implementation in Python
 
- 
+## @defgroup xl XL
+## @brief Python implementation
+## @{
+
+## @defgroup node Node types
+## @{
+
+class Node: pass
+
+## @defgroup leaf leaf
+## @brief leaf nodes
+## @{
+
+## leaf in the parse tree
+class Leaf(Node): pass
+
+## integer number
+class Integer(Leaf): pass
+
+## floating point number
+class Real(Leaf): pass
+
+## string
+class Text(Leaf): pass
+
+## names and symbols
+class Name(Leaf): pass
+
+## @}
+
+## @defgroup inner inner
+## @brief inner nodes (trees)
+## @{
+
+## structural trees
+class Inner(Node): pass
+
+## infix operrator
+class Infix(Inner): pass
+
+## prefix operator
+class Prefix(Inner): pass
+
+## postfix operator
+class Postfix(Inner): pass
+
+## curly block
+class Block(Inner): pass
+
+## @}
+
+## @}
+
+## @}
