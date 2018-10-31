6ad33620bf4a82c3f203a9089b77f317cf83265f
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-10-31 16:16:02 +0400

    + token:name
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- py/xl.py
+++ py/xl.py
@@ -12,27 +12,37 @@ import os,sys
 
 ## root class
 class Node:
+    
     ## construct leaf with value
     ## @param[in] V value
     def __init__(self,V):
         ## primitive value
         self.value = V
-        ## nested elements
+        ## nested elements = vector & stack
         self.nest = []
-    ## vector/stack
+        
+    # vector/stack
+    
+    ## `<<` operator
     def __lshift__(self,obj):
         return self.push(obj)
+    
+    ## push as stack
     def push(self,obj):
         self.nest.append(obj) ; return self
+        
     ## print
     def __repr__(self):
         return self.dump()
+    ## full tree dump
     def dump(self,depth=0):
         S = self.pad(depth) + self.head()
         for j in self.nest: S += j.dump(depth+1)
         return S
+    ## short header-only dump
     def head(self):
         return '<%s:%s>' % (self.__class__.__name__.lower(),self.value)
+    ## left pad with tabs (for tree)
     def pad(self,N):
         return '\n' + '\t'*N
 
@@ -120,6 +130,11 @@ def t_int(t):
     r'[0-9]+'
     t.value = Integer(int(t.value)) ; return t
     
+## name
+def t_name(t):
+    r'[a-zA-Z0-9_]+'
+    t.value = Name(t.value) ; return t
+    
 ## lexer error callback
 def t_error(t): raise SyntaxError(t)
 
@@ -131,42 +146,56 @@ lexer = lex.lex()
 ## @defgroup syntax syntax
 ## @{
 
+## empty file
 def p_none(p):
     ' REPL : '
 
+## drop newlines
 def p_nl(p):
     ' REPL : REPL nl'
 
+## recursive parser (sequence of **ex**pressions)
 def p_recur(p):
     ' REPL : REPL ex '
     print p[2]
-    
+
+## prefix expression
 def p_prefix(p):
     ' ex : op ex '
     p[0] = p[1] << p[2]
     
+## integer is expression
 def p_int(p):
     ' ex : int '
     p[0] = p[1]
     
+## real is expression
 def p_real(p):
     ' ex : real '
     p[0] = p[1]
     
+## text is expression
 def p_text(p):
     ' ex : text '
     p[0] = p[1]
     
+## name is expression
 def p_name(p):
     ' ex : name '
     p[0] = p[1]
     
+## syntax error callback
 def p_error(p): raise SyntaxError(p)
 
 ## parser
 parser = yacc.yacc(debug=False,write_tables=False)
 
 ## @}
+
+## @}
+
+## @defgroup startup Startup code
+## @{
 
 ## source code
 try:
